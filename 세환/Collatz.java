7096d7acddfec22cb49ccf8e3e47c515d435d497
ksh9241 <ksh9241@naver.com>
2021-07-22 23:30:31 +0900

    초보자의 알고리즘

---
+++ 세환/Collatz.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+       long a = num;
+		int answer = 0;
+		while(true) {
+			if(a == 1) {
+				break;
+			}
+			if(answer >= 500) {
+				answer = -1;
+				break;
+			}
+			if(a % 2 == 0) {
+				a /= 2;
+				answer++;
+			}else {
+				a = (a * 3) + 1;
+				answer++;
+			}
+		}
+        return answer;
+    }
+}
