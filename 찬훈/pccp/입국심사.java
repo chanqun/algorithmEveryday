5a03a524c7bca97ef7452db449a22b9d86ee8cfb
chanqun <cksgns93@naver.com>
2022-12-24 22:21:03 +0900

    입국심사

---
+++ 찬훈/pccp/입국심사.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = Long.MAX_VALUE;
+        
+        Arrays.sort(times);
+        
+        long left = times[0];
+        long right = (long)times[times.length - 1] * n;
+                
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+            
+            for (int time: times) {
+                sum += mid/time;
+            }
+            
+            if (sum>=n) {
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
