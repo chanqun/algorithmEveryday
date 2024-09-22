deb96937e6a51b59135015e2a9027f9139eb23ce
chanqun <cksgns93@naver.com>
2024-09-22 10:51:58 +0900

    동영상 재생기

---
+++ 찬훈/programmers/동양상재생기.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Solution {
+    private int videoLen;
+    private int posInt;
+    private int opStart;
+    private int opEnd;
+    
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String answer = "";
+        
+        videoLen = changeTimeSringToInt(video_len);
+        posInt = changeTimeSringToInt(pos);
+        opStart = changeTimeSringToInt(op_start);
+        opEnd = changeTimeSringToInt(op_end);
+        
+        for(String command: commands) {
+            doCommand(command);
+        }
+        
+        answer = makeIntToAnswer(posInt);
+        System.out.println(answer);
+        
+        return answer;
+    }
+    
+    private int changeTimeSringToInt(String time) {
+        String[] timeArray = time.split(":");
+        
+        return Integer.parseInt(timeArray[0]) * 60 + Integer.parseInt(timeArray[1]);
+    }
+    
+    private void doCommand(String command) {
+        if (posInt >= opStart && posInt <= opEnd) {
+            posInt = opEnd;
+        }
+        
+        if (command.equals("next")) {
+            int temp = posInt + 10;
+            
+            if (temp > videoLen) {
+                posInt = videoLen;
+            } else {
+                posInt = temp;                
+            }
+        } else if (command.equals("prev")) {
+            int temp = posInt - 10;
+            
+            if (temp < 0) {
+                posInt = 0;
+            } else {
+                posInt = temp;                
+            }
+        }
+        
+        if (posInt >= opStart && posInt <= opEnd) {
+            posInt = opEnd;
+        }
+    }
+    
+    private String makeIntToAnswer(int num) {
+        int hour = num / 60;
+        int min = num % 60;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        if (hour < 10) {
+            sb.append("0");
+        }
+        
+        sb.append(hour + ":");
+        
+        if (min < 10) {
+            sb.append("0");
+        }
+        
+        sb.append(min);
+        
+        return sb.toString();
+    }
+}
